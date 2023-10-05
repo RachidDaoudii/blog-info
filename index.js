21a519232fd6074665abd023f5582c4676232097
Tharbouch <112890451+soufianeargane@users.noreply.github.com>
2023-10-05 16:33:08 +0100

    added session message with express-session

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const session = require('express-session');
 const app = express();
 const userRoutes = require('./routes/profile');
 const articleRoutes = require('./routes/articles');
@@ -14,8 +15,14 @@ const ejs = require('ejs');
 app.use(express.json()); // Parse JSON requests
 app.use(express.urlencoded({ extended: true })); // Parse URL-encoded requests
 
-
-
+// Configure session middleware
+app.use(
+    session({
+        secret: 'any secret key', // hadi khassha tbdel darori bch t3ml session dyalk secure
+        resave: false,
+        saveUninitialized: true,
+    })
+);
 
 // Set EJS as the view engine
 app.set('view engine', 'ejs');
