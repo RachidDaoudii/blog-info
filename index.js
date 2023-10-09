feb6b8c9a5430025adbd1bd399778aaa302c03a7
Tharbouch <112890451+soufianeargane@users.noreply.github.com>
2023-10-09 16:03:32 +0100

    added token but manually

--- index.js
+++ index.js
@@ -1,31 +1,26 @@
-const express = require('express');
-const session = require('express-session');
-const app = express();
-const userRoutes = require('./routes/profile');
-const articleRoutes = require('./routes/articles');
-const methodOverride = require('method-override');
-const csrf = require('csurf');
-const cookieParser = require('cookie-parser');
-app.use(cookieParser());
+const userRoutes          = require('./routes/profile');
+const articleRoutes       = require('./routes/articles');
+const generateRandomToken = require('./helpers/tokenGenerator');
+const bodyParser          = require('body-parser');
 
-// Initialize the CSRF middleware and make it available to your routes
-const csrfProtection = csrf({ cookie: true });
-app.use(csrfProtection);
+const express        = require('express');
+const session        = require('express-session');
+const app            = express();
+const methodOverride = require('method-override');
+const csrf           = require('csurf');
+const cookieParser   = require('cookie-parser');
+const http           = require('http');
+const cookie         = require('cookie');
+app.use(cookieParser());
+// app.use(bodyParser.urlencoded({ extended: false }));
+
+
 
 // Use method-override middleware
 app.use(methodOverride('_method'));
-// set style
 app.use(express.static('public'));
-
-
-const port = 3000;
-
-const ejs = require('ejs');
-
-// Parse JSON and URL-encoded bodies
 app.use(express.json()); // Parse JSON requests
 app.use(express.urlencoded({ extended: true })); // Parse URL-encoded requests
-
 // Configure session middleware
 app.use(
     session({
@@ -34,15 +29,9 @@ app.use(
         saveUninitialized: true,
     })
 );
-
 // Set EJS as the view engine
 app.set('view engine', 'ejs');
 app.set('views', './public/views');
-
-app.use((req, res, next) => {
-    res.locals.csrfToken = req.csrfToken();
-    next();
-});
 
 
 
@@ -51,4 +40,11 @@ app.use((req, res, next) => {
 app.use('/user', userRoutes);
 app.use('/article', articleRoutes);
 
+
+
+
+
+
+
+const port = 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
--- routes/profile.js
+++ routes/profile.js
@@ -2,12 +2,13 @@ const express = require('express');
 const router = express.Router();
 const UserController = require('../controllers/userProfileController');
 const upload = require('../helpers/imageHelper');
+const generateCSRFToken = require("../helpers/tokenGenerator");
+const checkCsrfToken = require("../middlewares/checkCsrfToken");
+
 
 // Display user profile
-router.get('/profile/:userId', UserController.getUserProfile);
+router.get('/profile/:userId', generateCSRFToken, UserController.getUserProfile);
 // Update user profile
-router.post('/profile/:userId', upload.upload.single('profileImage') ,UserController.updateUserProfile);
-
-
+router.post('/profile/:userId' ,upload.upload.single('profileImage') ,UserController.updateUserProfile);
 
 module.exports = router;
