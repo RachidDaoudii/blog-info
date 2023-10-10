98f891fe7013af3d4c0cb9a6c9574caae349f040
Tharbouch <112890451+soufianeargane@users.noreply.github.com>
2023-10-10 16:26:16 +0100

    added id for header

--- index.js
+++ index.js
@@ -32,10 +32,12 @@ app.use(layoutEjs);
 app.set("view engine", "ejs");
 app.set("views", "./public/views");
 
-// app.use((req, res, next) => {
-//     res.locals.csrfToken = req.csrfToken();
-//     next();
-// });
+app.use((req, res, next) => {
+    let loggedInUser = req.cookies.loggedIn_user;
+    res.locals.loggedInUser = loggedInUser || null;
+    next();
+});
+
 
 // routes
 app.use("/user", userRoutes);
