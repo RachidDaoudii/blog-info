a5a8977a7f098abfc6bc28afa54f09a76dc92886
rachiddaoudi <rachiddaoudi533@gmail.com>
2023-10-11 09:29:43 +0100

    validation form csrf token

--- routes/articles.js
+++ routes/articles.js
@@ -4,21 +4,17 @@ const ControllerArticle = require("../controllers/articles.Controller");
 const imageHelper = require("../helpers/imageHelper");
 
 const isAuth = require("../middlewares/isAuthenticated");
-
-// const checkTekon = (req, res, next)=>{
-//     res.locals.csrfToken = req.csrfToken();
-//     next();
-// }
+const generateCSRFToken = require('../helpers/tokenGenerator')
 
 router.get("/", ControllerArticle.index);
-router.get("/add", isAuth.isAuthenticated, ControllerArticle.add);
+router.get("/add", isAuth.isAuthenticated, generateCSRFToken, ControllerArticle.add);
 router.post(
   "/add",
   imageHelper.upload.single("image"),
   ControllerArticle.store
 );
 router.get("/show/:id", ControllerArticle.show);
-router.get("/edit/:id", isAuth.isAuthenticated, ControllerArticle.edit);
+router.get("/edit/:id", isAuth.isAuthenticated, generateCSRFToken, ControllerArticle.edit);
 router.post(
   "/update/:id",
   isAuth.isAuthenticated,
@@ -26,6 +22,6 @@ router.post(
   ControllerArticle.update
 );
 router.delete("/delete/:id", isAuth.isAuthenticated, ControllerArticle.delete);
-router.get("/dashborad", isAuth.isAuthenticated, ControllerArticle.dashboard);
+router.get("/dashborad", isAuth.isAuthenticated, generateCSRFToken, ControllerArticle.dashboard);
 
 module.exports = router;
